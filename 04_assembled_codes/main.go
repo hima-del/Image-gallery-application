8837ac136a64b9fea79277892dbe51ce58164405
hima-del <himajaharidas77@gmail.com>
2020-10-27 16:00:16 +0530

    signup getimages routes assembled

---
+++ 04_assembled_codes/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "postgres://himaja:password@localhost/image_gallery?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("you are connected to database")
+}
+
+func main() {
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/api/images/", getImages)
+	http.ListenAndServe(":8080", nil)
+}
