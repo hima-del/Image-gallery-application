184c97b6187dcf8ae6881c5f4e9dd63bd5eca84a
hima-del <himajaharidas77@gmail.com>
2020-10-30 00:29:30 +0530

    no logout

--- 03_others/02/login-refresh/auth1.go
+++ 03_others/02/login-refresh/auth1.go
@@ -10,144 +10,17 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
-
-	uuid "github.com/satori/go.uuid"
 
 	"github.com/dgrijalva/jwt-go"
 
-	"golang.org/x/crypto/bcrypt"
-
 	_ "github.com/lib/pq"
 )
-
-var db *sql.DB
-
-func init() {
-	var err error
-	db, err = sql.Open("postgres", "postgres://himaja:password@localhost/image_gallery?sslmode=disable")
-	if err != nil {
-		panic(err)
-	}
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("you are connected to database")
-}
-
-func main() {
-	http.HandleFunc("/signup", signup)
-	http.HandleFunc("/login", login)
-	http.HandleFunc("/api/images/id/", getImage)
-	http.HandleFunc("/api/images/", getImages)
-	http.HandleFunc("/api/images/", createImage)
-	http.HandleFunc("/api/images/id/", deleteImage)
-	http.ListenAndServe(":8080", nil)
-}
-
-type Credentials struct {
-	ID       uint64 `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
 
 type Image struct {
 	ID        int    `json:"id"`
 	Label     string `json:"label"`
 	Userid    int    `json:"userid"`
 	Imagename string `json:"imagename"`
-}
-
-type TokenDetails struct {
-	AccessToken  string
-	RefreshToken string
-	AccessUUID   string
-	RefreshUUID  string
-	ATExpires    int64
-	RTExpires    int64
-}
-
-func signup(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		b, err := ioutil.ReadAll(req.Body)
-		defer req.Body.Close()
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		var creds Credentials
-		err = json.Unmarshal(b, &creds)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		hashedpassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 8)
-		_, err = db.Query("insert into userdetails (id,username,password)values ($1,$2,$3)", creds.ID, creds.Username, string(hashedpassword))
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		token, err := createToken(creds.ID)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		tokens := map[string]string{
-			"acces_token":   token.AccessToken,
-			"refresh_token": token.RefreshToken,
-		}
-		data, err := json.Marshal(tokens)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Write(data)
-	}
-}
-
-func login(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		creds := &Credentials{}
-		err := json.NewDecoder(req.Body).Decode(creds)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		result := db.QueryRow("select password from userdetails where username=$1", creds.Username)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		storedCreds := &Credentials{}
-		err = result.Scan(&storedCreds.Password)
-		if err != nil {
-			if err == sql.ErrNoRows {
-				w.WriteHeader(http.StatusUnauthorized)
-				return
-			}
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if err = bcrypt.CompareHashAndPassword([]byte(storedCreds.Password), []byte(creds.Password)); err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
-		}
-		token, err := createToken(creds.ID)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		tokens := map[string]string{
-			"acces_token":   token.AccessToken,
-			"refresh_token": token.RefreshToken,
-		}
-		data, err := json.Marshal(tokens)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Write(data)
-	}
 }
 
 func getImage(w http.ResponseWriter, req *http.Request) {
@@ -231,7 +104,6 @@ func getImages(w http.ResponseWriter, req *http.Request) {
 
 func createImage(w http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
-
 		err := tokenValid(req)
 		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
@@ -281,40 +153,18 @@ func deleteImage(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func createToken(userid uint64) (*TokenDetails, error) {
-	var err error
-	td := &TokenDetails{}
-	td.ATExpires = time.Now().Add(time.Minute * 15).Unix()
-	AccessID, err := uuid.NewV4()
-	td.AccessUUID = AccessID.String()
-	RefreshID, err := uuid.NewV4()
-	td.RefreshUUID = RefreshID.String()
-
-	//creating access token
-	os.Setenv("ACCESS_SECRET", "lkrdjgjzjlgj")
-	atClaims := jwt.MapClaims{}
-	atClaims["authorized"] = true
-	atClaims["access_uuid"] = td.AccessUUID
-	atClaims["user_id"] = userid
-	atClaims["exp"] = td.ATExpires
-	pointerToAccessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-	td.AccessToken, err = pointerToAccessToken.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
-	if err != nil {
-		return nil, err
+func logout(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		err := tokenValid(req)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		//bearToken := req.Header.Get("Authorization")
+		//fmt.Println(bearToken)
+		req.Header.Del("Authorization")
+		fmt.Println("logged out")
 	}
-
-	//creating refresh token
-	os.Setenv("REFRESH_SECRET", "jgsuygsghbb")
-	rtClaims := jwt.MapClaims{}
-	rtClaims["refresh_uuid"] = td.RefreshUUID
-	atClaims["user_id"] = userid
-	atClaims["exp"] = td.RTExpires
-	pointerToRefreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
-	td.RefreshToken, err = pointerToRefreshToken.SignedString([]byte(os.Getenv("REFRESH_SECRET")))
-	if err != nil {
-		return nil, err
-	}
-	return td, nil
 }
 
 func extractToken(req *http.Request) string {
---
+++ 03_others/02/login-refresh/main.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+
+	"github.com/dgrijalva/jwt-go"
+
+	"golang.org/x/crypto/bcrypt"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "postgres://himaja:password@localhost/image_gallery?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("you are connected to database")
+}
+
+func main() {
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/api/images/id/", getImage)
+	http.HandleFunc("/api/images/", getImages)
+	http.HandleFunc("/api/images", createImage)
+	http.HandleFunc("/api/images/id", deleteImage)
+	http.HandleFunc("/logout", logout)
+	http.ListenAndServe(":8080", nil)
+}
+
+type Credentials struct {
+	ID       uint64 `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type TokenDetails struct {
+	AccessToken  string
+	RefreshToken string
+	AccessUUID   string
+	RefreshUUID  string
+	ATExpires    int64
+	RTExpires    int64
+}
+
+func signup(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		b, err := ioutil.ReadAll(req.Body)
+		defer req.Body.Close()
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		var creds Credentials
+		err = json.Unmarshal(b, &creds)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		hashedpassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 8)
+		_, err = db.Query("insert into userdetails (id,username,password)values ($1,$2,$3)", creds.ID, creds.Username, string(hashedpassword))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		token, err := createToken(creds.ID)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		tokens := map[string]string{
+			"acces_token":   token.AccessToken,
+			"refresh_token": token.RefreshToken,
+		}
+		data, err := json.Marshal(tokens)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(data)
+	}
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		creds := &Credentials{}
+		err := json.NewDecoder(req.Body).Decode(creds)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		result := db.QueryRow("select password from userdetails where username=$1", creds.Username)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		storedCreds := &Credentials{}
+		err = result.Scan(&storedCreds.Password)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if err = bcrypt.CompareHashAndPassword([]byte(storedCreds.Password), []byte(creds.Password)); err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+		}
+		beartoken := req.Header.Get("Authorization")
+		if beartoken != "" && len(beartoken) == 162 {
+			newAccesstoken, err := createAccessToken(creds.ID)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			newToken := map[string]string{
+				"access_token": newAccesstoken.AccessToken,
+			}
+			tokenData, err := json.Marshal(newToken)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			w.Write(tokenData)
+		} else if beartoken == "" {
+			token, err := createToken(creds.ID)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			tokens := map[string]string{
+				"acces_token":   token.AccessToken,
+				"refresh_token": token.RefreshToken,
+			}
+			data, err := json.Marshal(tokens)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			w.Write(data)
+		}
+	}
+}
+
+func createToken(userid uint64) (*TokenDetails, error) {
+	var err error
+	td := &TokenDetails{}
+	td.ATExpires = time.Now().Add(time.Minute * 15).Unix()
+	td.RTExpires = time.Now().Add(time.Hour * 24 * 7).Unix()
+	AccessID, err := uuid.NewV4()
+	td.AccessUUID = AccessID.String()
+	RefreshID, err := uuid.NewV4()
+	td.RefreshUUID = RefreshID.String()
+
+	//creating access token
+	os.Setenv("ACCESS_SECRET", "accesskey")
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["access_uuid"] = td.AccessUUID
+	atClaims["user_id"] = userid
+	atClaims["exp"] = td.ATExpires
+	pointerToAccessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	td.AccessToken, err = pointerToAccessToken.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+
+	//creating refresh token
+	os.Setenv("REFRESH_SECRET", "refreshkey")
+	rtClaims := jwt.MapClaims{}
+	rtClaims["refresh_uuid"] = td.RefreshUUID
+	atClaims["user_id"] = userid
+	atClaims["exp"] = td.RTExpires
+	pointerToRefreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	td.RefreshToken, err = pointerToRefreshToken.SignedString([]byte(os.Getenv("REFRESH_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+	return td, nil
+}
+
+func createAccessToken(userid uint64) (*TokenDetails, error) {
+	var err error
+	at := &TokenDetails{}
+	at.ATExpires = time.Now().Add(time.Minute * 15).Unix()
+	AccessID, err := uuid.NewV4()
+	at.AccessUUID = AccessID.String()
+	//creating new access token
+	os.Setenv("ACCESS_SECRET", "newaccesssecret")
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["access_uuid"] = at.AccessUUID
+	atClaims["user_id"] = userid
+	atClaims["exp"] = at.ATExpires
+	pointerToAccessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	at.AccessToken, err = pointerToAccessToken.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+	return at, nil
+}
