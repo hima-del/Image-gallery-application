a06cda87ba637f5f0cb40615c839dbde1ac3dd83
hima-del <himajaharidas77@gmail.com>
2020-11-06 10:58:45 +0530

    image uploading

--- 08_image_uploading/02/main.go
+++ 08_image_uploading/02/main.go
@@ -12,28 +12,31 @@ func main() {
 }
 
 func uploadFile(w http.ResponseWriter, req *http.Request) {
-	req.ParseMultipartForm(10 << 20)
-	file, header, err := req.FormFile("myfile")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
-	fmt.Println("uploaded file:", header.Filename)
-	fmt.Println("file size:", header.Size)
-	fmt.Println("MIME header:", header.Header)
+	if req.Method == http.MethodPost {
+		req.ParseMultipartForm(10 << 20)
+		file, header, err := req.FormFile("myfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Println("uploaded file:", header.Filename)
+		fmt.Println("file size:", header.Size)
+		fmt.Println("MIME header:", header.Header)
 
-	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer tempFile.Close()
+		tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer tempFile.Close()
 
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
-	}
-	tempFile.Write(fileBytes)
-	fmt.Fprintln(w, "successfully uploaded file")
+		fileBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			fmt.Println(err)
+		}
+		tempFile.Write(fileBytes)
+		fmt.Fprintln(w, "successfully uploaded file")
 
+		//To store image in database
+	}
 }
